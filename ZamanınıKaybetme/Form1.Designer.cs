3d18136d45c587dc74055f05cf58d3a9210eadad
Yuşa <aaslanmirza23@gmail.com>
2022-11-18 19:02:00 +0300

    Initial commit

---
+++ ZamanınıKaybetme/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ZamanınıKaybetme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 506);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Yenile";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(294, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(516, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pişman Olacağımız yarınların dünündeyiz;\r\nve yarın yeniden nedamet getirmemek içi" +
+    "n bugünün kıymetini bilmeliyiz.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 109);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1081, 475);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            this.chart1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseClick);
+            this.chart1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseMove);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "Zamanını Kaybetme";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1105, 596);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zamanını Kaybetme!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ClientSizeChanged += new System.EventHandler(this.Form1_ClientSizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ ZamanınıKaybetme/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ZamanınıKaybetme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ProgramGetir();
+            notifyIcon1.Visible = true;
+            notifyIcon1.Icon = SystemIcons.Application;
+            notifyIcon1.BalloonTipTitle = "Zamanını Kaybetme";
+            notifyIcon1.BalloonTipText = "Zamanını Kaybetme";
+            notifyIcon1.ShowBalloonTip(1000);
+           // this.Hide();
+        }
+
+        Dictionary<string, int> harcananBilgi = new Dictionary<string, int>();
+
+        async void ProgramGetir()
+        {
+            await Task.Run(async () =>
+            {
+                while (true)
+                {
+                    Stopwatch watch = new Stopwatch();
+                    watch.Start();
+                    string aktifUyg = getActiveWindowName();  
+                    if (aktifUyg!=null)
+                    {
+                        if (harcananBilgi.ContainsKey(aktifUyg.Trim()))
+                        {
+                            harcananBilgi[aktifUyg] += 1;
+                        }
+                        else
+                        {
+                            harcananBilgi.Add(aktifUyg.Trim(), 1);
+                        }
+                        watch.Stop();
+                        double beklemeSuresi = 1000 - watch.Elapsed.Milliseconds;
+                        await Task.Delay(Convert.ToInt32(beklemeSuresi));
+                    }
+                    
+                }
+               
+            });
+
+
+        }
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        private static extern IntPtr GetForegroundWindow();
+
+        public string getActiveWindowName()
+        {
+            try
+            {
+                var activatedHandle = GetForegroundWindow();
+
+                Process[] processes = Process.GetProcesses();
+                foreach (Process clsProcess in processes)
+                {
+                    if (activatedHandle == clsProcess.MainWindowHandle)
+                    {
+                        string processName = clsProcess.ProcessName;
+
+                        return processName;
+                    }
+                }
+            }
+            catch { }
+            return null;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                button1.Enabled = false;
+                try
+                {
+                    foreach (var series in chart1.Series)
+                    {
+                        series.Points.Clear();
+                    }
+
+                }
+                catch 
+                {
+                }
+               
+            bas:
+                try
+                {
+                    foreach (var item in harcananBilgi)
+                    {
+                        if (this.InvokeRequired)
+                        {
+                            this.Invoke((MethodInvoker)delegate ()
+                            {
+                                chart1.Series["Series1"].Points.AddXY(item.Key, item.Value);
+
+                            });
+                        }
+
+                    }
+
+                }
+                catch 
+                {
+                    goto bas;
+                }
+
+            });
+            button1.Enabled = true;
+           
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void chart1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Chart pie = (Chart)sender;
+            int pointIndex = pieHitPointIndex(pie, e);
+            if (pointIndex >= 0)
+            {
+                DataPoint dp = pie.Series[0].Points[pointIndex];
+                TimeSpan result = TimeSpan.FromSeconds(dp.YValues[0]);
+                MessageBox.Show("Kullanım Süreniz: " + new DateTime(result.Ticks).ToString("HH:mm:ss"),"Bilgilendirme",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+        }
+
+        private void chart1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Chart pie = (Chart)sender;
+            int pointIndex = pieHitPointIndex(pie, e);
+            if (pointIndex >= 0)
+            {
+                Cursor = Cursors.Hand;
+            }
+            else
+            {
+                Cursor = Cursors.Default;
+            }
+        }
+        private int pieHitPointIndex(Chart pie, MouseEventArgs e)
+        {
+            HitTestResult hitPiece = pie.HitTest(e.X, e.Y, ChartElementType.DataPoint);
+            HitTestResult hitLegend = pie.HitTest(e.X, e.Y, ChartElementType.LegendItem);
+            int pointIndex = -1;
+            if (hitPiece.Series != null) pointIndex = hitPiece.PointIndex;
+            if (hitLegend.Series != null) pointIndex = hitLegend.PointIndex;
+            return pointIndex;
+        }
+
+        private void Form1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+    }
+}
